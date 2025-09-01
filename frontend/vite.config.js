7461da6b480d3f07e4da0256ad23362c246d9c33
Aayush Vyas <aayush25vyas@gmail.com>
2025-09-02 03:46:30 +0530

    Added Absolute configuration using viteconfig and jsconfig

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve:{
+    alias:{
+      src:"/src",
+      components: "/src/Components",
+      pages:"/src/Pages",
+      config:"/src/Config",
+      helpers:"/src/Helpers",
+      layout:"/src/Layouts",
+      assests:"/src/Assests",
+      redux:"/src/Redux",
+      routes:"/src/Routes"
+
+    }
+  }
 })
